da436ceb94038e3a3a65bcad1d57f5abcf3cce6e
ThyagoMartins1 <oficialthyago2@gmail.com>
2024-05-21 15:54:45 -0300

    #

---
+++ app/Http/Controllers/api/Serviceswork/CustomrequestController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\api\Serviceswork;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CustomrequestController extends Controller
+{
+    //
+}
